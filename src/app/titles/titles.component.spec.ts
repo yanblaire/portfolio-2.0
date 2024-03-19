5bbe679d090ce8bb29dd775c8c6e9a775918f29d
Yan Blaire Dologuin <yan.dologuin@nwktc.edu>
2024-03-19 10:51:09 -0600

    Added top, about me and titles

---
+++ src/app/titles/titles.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TitlesComponent } from './titles.component';
+
+describe('TitlesComponent', () => {
+  let component: TitlesComponent;
+  let fixture: ComponentFixture<TitlesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TitlesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TitlesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/titles/titles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titles',
+  templateUrl: './titles.component.html',
+  styleUrl: './titles.component.css'
+})
+export class TitlesComponent {
+
+}
